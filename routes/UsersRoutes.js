fd221a64a71e49df47c01450bc143ed1da4ebaab
danyentezari <dany.entezari@gmail.com>
2020-06-27 15:46:09 +0400

    Encryption for user password.

--- routes/UsersRoutes.js
+++ routes/UsersRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const router = express.Router();
 const UsersModel = require('../models/UsersModel');
 
@@ -13,20 +14,45 @@ router.post(
             password: req.body.password
         };
 
-        const newUsersModel = new UsersModel(formData);
-        newUsersModel.save(
-            (err, dbResult) => {
 
-                // If something goes wrong, send error
-                if(err) {
-                    res.send(err);
-                } 
-                // Otherwise, send success message
-                else {
-                    res.send("User has been saved")
-                }
+        // Step 1) Generate a salt
+        bcrypt.genSalt(
+            (err, salt) => {
+
+                // Step 2) Generate a hash
+                bcrypt.hash(
+                    formData.password, // first ingredient
+                    salt, // second ingredient
+                    (err, hashedPassword) => {
+
+                        const newUsersModel = new UsersModel(formData);
+
+                        // Step 3) Replace the original password with hash
+                        newUsersModel.password = hashedPassword;
+
+                        // Step 4) Save user data to database (with encrypted password)
+                        newUsersModel.save(
+                            (err, dbResult) => {
+
+                                // If something goes wrong, send error
+                                if(err) {
+                                    res.send(err);
+                                }
+                                // Otherwise, send success message
+                                else {
+                                    res.send("User has been saved")
+                                }
+                            }
+                        );
+                        
+                    }
+                )
             }
         );
+
+
+
+        
 
         
     }
